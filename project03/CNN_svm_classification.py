676c5b916c7ee5012a1ae947d9803277f139dab9
Clair Green <clair.green2048@gmail.com>
2026-04-29 14:09:41 -0400

    attempting to work project 4

--- project03/CNN_svm_classification.py
+++ project03/CNN_svm_classification.py
@@ -1,7 +1,11 @@
 import os
 import cv2
+import torch
+import argparse
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import Image
 from preprocessing import read_json_data, preprocess_one_image
 from segmentation_anything import segment_one_image
 from feature_extraction_sift import extract_sift_features_one_image
@@ -10,14 +14,72 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from segment_anything import sam_model_registry, SamPredictor
-from torchvision import models
+from torchvision import models, transforms
+from torch.utils.data import Dataset, DataLoader
 from constants import (
     IMAGES_DIR,
     RAW_DIR,
+    CROPPED_DIR, 
+    PREPROCESSED_DIR,
     SAM_CHECKPOINT,
     SAM_MODEL_TYPE,
-    SIFT_FEATURES_DIR
 )
+
+class HoldPreprocessedDataset(Dataset):
+    def __init__(self, samples, use_segmentation=False, predictor=None):
+        self.samples = samples
+        self.use_segmentation = use_segmentation
+        self.predictor = predictor
+        self.to_tensor = transforms.ToTensor()
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        sample = self.samples[idx]
+        img = sample["img"]
+        metadata = sample["metadata"]
+        label = sample["label_value"]
+
+        if self.use_segmentation:
+            img, _ = segment_one_image(
+                img,
+                self.predictor,
+                metadata["new_h"],
+                metadata["new_w"],
+                metadata["y_offset"],
+                metadata["x_offset"],
+            )
+
+        # OpenCV is BGR; torchvision expects RGB-like ordering
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # ToTensor gives float tensor in [0,1], shape [C,H,W]
+        img_tensor = self.to_tensor(img)
+        label_tensor = torch.tensor(label, dtype=torch.long)
+
+        return img_tensor, label_tensor
+
+class CNNFeatureExtractor(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 16, 5, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, 5, padding=2)
+        self.conv3 = nn.Conv2d(32, 64, 5, padding=2)
+        self.conv4 = nn.Conv2d(64, 128, 5, padding=2)
+        self.conv4_drop = nn.Dropout2d(p=0.25)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = F.relu(F.max_pool2d(self.conv4(x), 2))
+        x = self.conv4_drop(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        return torch.flatten(x, 1)
+    
+
 
 def clamp_bbox(left: int, top: int, width: int, height: int, img_w: int, img_h: int) -> tuple[int, int, int, int]:
     """
@@ -54,7 +116,7 @@ def crop_image(
 
     raw_w, raw_h = img.size
     left, top, width, height = clamp_bbox(bbox_left, bbox_top, bbox_width, bbox_height, raw_w, raw_h)
-    left, top, width, height = pad_and_clamp_bbox(left, top, width, height, 4, raw_w, raw_h)
+    left, top, width, height = pad_and_clamp_bbox(left, top, width, height, pad, raw_w, raw_h)
     out = {
         "bbox_left": left,
         "bbox_top": top, 
@@ -78,7 +140,9 @@ def crop_image(
     else:
         raise ValueError(f"Unsupported exif_rotation: {rotation}")
     
-    return cv2.imread(cropped), out
+    cropped = np.array(cropped)
+    cropped = cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR)
+    return cropped, out
 
 def normalize_img_size(img, size=256):
     h, w = img.shape[:2]
@@ -97,11 +161,11 @@ def normalize_img_size(img, size=256):
     canvas[y_offset:y_offset + new_h, x_offset:x_offset + new_w] = resized
 
     resize_meta = {
-        canvas_size=size,
-        new_h=new_h,
-        new_w=new_w,
-        y_offset=y_offset,
-        x_offset=x_offset,
+        "canvas_size": size,
+        "new_h": new_h,
+        "new_w":new_w,
+        "y_offset": y_offset,
+        "x_offset": x_offset,
     }
     return canvas, resize_meta
 
@@ -155,6 +219,7 @@ def get_image_segmentation(img, mask, h_offset, w_offset):
     roi = result_img[h_offset:h_offset+mask_h, w_offset:w_offset+mask_w]
     roi[mask == 0] = (0,0,0) 
     result_mask[h_offset:h_offset+mask_h, w_offset:w_offset+mask_w] = mask
+    return roi, result_mask
 
 def segment_one_image(img, predictor, new_h, new_w, h_offset, w_offset, pad=5):
     roi = get_roi_from_preprocessed(img, new_h, new_w, h_offset, w_offset)
@@ -192,10 +257,10 @@ def segment_one_image(img, predictor, new_h, new_w, h_offset, w_offset, pad=5):
     final_img = cv2.cvtColor(segmented_img, cv2.COLOR_RGB2BGR)
     return final_img, resized_mask
 
-
 # add train CNN function later
 
 
+# Get CNN features portion
 def get_cnn_features_SVM(model, dataloader, device):
     model.eval()
     model.to(device)
@@ -214,49 +279,193 @@ def get_cnn_features_SVM(model, dataloader, device):
     return np.vstack(all_features), np.concatenate(all_labels)
 
 
-
-
-
-
-
-
 def build_feature_extractor_RESNET():
     model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
     model = nn.Sequential(*list(model.children())[:-1])  # remove final FC
     return model
 
+def train_model(model, train_loader, val_loader, device, epochs=10):
+    for param in model.parameters():
+        param.requires_grad = False
 
-def run_svm_classification(X, Y):
-    X_train, X_temp, Y_train, Y_temp = train_test_split(
-        X, Y,
-        test_size=0.3,
-        random_state=42,
-        stratify=Y   
-    )
+    for param in model.fc.parameters():
+        param.requires_grad = True
 
+    train_losses = []
+    train_accs = []
+    val_accs = []
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.fc.parameters(), lr=1e-3)
+
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        correct = 0
+        total = 0
+
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)  
+            loss = criterion(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+
+        epoch_loss = running_loss / len(train_loader)
+        train_acc = 100. * correct / total
+
+        model.eval()
+        correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for images, labels in val_loader:
+                outputs = model.forward(images)                     # returns (batch_size, 10) holding the output prob for every class in every image
+                predictions = torch.argmax(outputs, dim=1)          # grabs the max prob reducing along each row to get (batch_size, 1)
+                correct += torch.sum(predictions == labels).float() # sums the correct predictions and turns to float for calculation later
+                total += images.shape[0]                             # grabs the total number of images in this batch
+
+        val_acc = 100. * correct / total
+
+        train_losses.append(epoch_loss)
+        train_accs.append(train_acc)
+        val_accs.append(val_acc)
+
+        if (epoch + 1) % 50 == 0:
+            print(f'Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss:.4f}, '
+                f'Train Acc: {train_acc:.2f}%, Val Acc: {val_acc:.2f}%')
+    
+    return train_losses, train_accs, val_accs
+
+
+def run_svm_classification(X_train, Y_train, X_val, Y_val, kernel="rbf"):
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
-
-    X_val, X_test, Y_val, Y_test = train_test_split(
-        X_temp, Y_temp,
-        test_size=0.5,
-        random_state=42,
-        stratify=Y_temp
-    )
-
-    X_test = scaler.fit_transform(X_test)
+    X_val = scaler.fit_transform(X_val)
 
     # Change to linear to see other results
-    clf = svm.SVC(kernel='rbf', C=1.0, gamma='scale')
+    clf = svm.SVC(kernel=kernel, C=1.0, gamma='scale')
     clf.fit(X_train, Y_train)
 
     Y_pred = clf.predict(X_val)
 
     accuracy = np.mean(Y_pred == Y_val)
     print(f"SVM Classification Accuracy: {accuracy:.4f}")
-    print(f"Classification Report:\n{classification_report(Y_test, Y_pred)}")
+    print(f"Classification Report:\n{classification_report(Y_val, Y_pred)}")
 
     # UNCOMMENT ONLY WHEN DOING FINAL TEST ON CHOSEN MODEL
     # Y_test_pred = clf.predict(X_test)
     # test_accuracy = np.mean(Y_test_pred == Y_test)
     # print(f"Test Accuracy: {test_accuracy:.4f}")
+
+if __name__ == "__main__":
+    num_classes = 7
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model")
+    args = parser.parse_args()
+
+    data_path = os.path.join(RAW_DIR, "annotations.json")
+    data = read_json_data(data_path)
+
+    os.makedirs(os.path.join(CROPPED_DIR, IMAGES_DIR), exist_ok=True)
+    os.makedirs(os.path.join(PREPROCESSED_DIR, IMAGES_DIR), exist_ok=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    preprocessed_data = []
+
+
+    is_sam_model = args.model in ["1", "2"]  
+    predictor = None
+    if is_sam_model:
+        sam = sam_model_registry[SAM_MODEL_TYPE](checkpoint=SAM_CHECKPOINT)
+        sam.to(device=device)
+        predictor = SamPredictor(sam)
+
+    for record in data:
+        img_src = os.path.join(RAW_DIR, IMAGES_DIR, record["filename"])
+
+        img, metadata = preprocess_one_image(
+            img_src=img_src,
+            bbox_left=int(record["bbox_left"]),
+            bbox_top=int(record["bbox_top"]),
+            bbox_height=int(record["bbox_height"]),
+            bbox_width=int(record["bbox_width"]),
+            rotation=int(record["exif_rotation"]),
+            label_name=record["label_name"],
+            label_value=int(record["label_value"])
+        )
+
+        preprocessed_data.append({
+            "img": img,
+            "metadata": metadata,
+            "label_value": metadata["label_value"]
+        })
+
+    print("Preprocessed data")
+
+    labels = [sample["label_value"] for sample in preprocessed_data]
+    train_samples, temp_samples = train_test_split(
+        preprocessed_data,
+        test_size=0.3,
+        random_state=42,
+        stratify=labels
+    )
+    temp_labels = [sample["label_value"] for sample in temp_samples]
+    val_samples, test_samples = train_test_split(
+        temp_samples,
+        test_size=0.5,
+        random_state=42,
+        stratify=temp_labels
+    )
+
+    train_dataset = HoldPreprocessedDataset(
+        samples=train_samples,
+        use_segmentation=is_sam_model,
+        predictor=predictor
+    )
+    val_dataset = HoldPreprocessedDataset(
+        samples=val_samples,
+        use_segmentation=is_sam_model,
+        predictor=predictor
+    )
+    test_dataset = HoldPreprocessedDataset(
+        samples=test_samples,
+        use_segmentation=is_sam_model,
+        predictor=predictor
+    )
+    batch_size = 32
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    print("Created datasets and sampled")
+
+    # SVM based classification
+    if args.model == "1":
+        model = build_feature_extractor_RESNET()
+        X_train, Y_train = get_cnn_features_SVM(model, train_loader, device=device)
+        X_val, Y_val = get_cnn_features_SVM(model, val_loader, device=device)
+
+        clf, Y_pred = run_svm_classification(X_train, Y_train, X_val, Y_val, kernel="linear")
+    
+    if args.model == "2":
+        model = build_feature_extractor_RESNET()
+        X_train, Y_train = get_cnn_features_SVM(model, train_loader, device=device)
+        X_val, Y_val = get_cnn_features_SVM(model, val_loader, device=device)
+
+        clf, Y_pred = run_svm_classification(X_train, Y_train, X_val, Y_val, kernel="rbf")
+
+
+    if args.model == "3":
+        model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(in_features, num_classes)
+        train_losses, train_accs, val_accs = train_model(model, train_loader, val_loader, device, epochs=10)
